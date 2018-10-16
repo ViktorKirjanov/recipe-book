dc7bd9973ebc3591e7acf4afa7bc7cf8e02bba92
Viktor <viktor.kirjanov@hashcoins.com>
2018-10-17 00:23:18 +0300

    Protecting Lazy Loaded Routes with canLoad

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,10 +5,11 @@ import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 import { SignupComponent } from './auth/signup/signup.component';
 import { SigninComponent } from './auth/signin/signin.component';
 import { HomeComponent } from './home/home.component';
+import { AuthGuardService } from './shared/services/auth-guard.service';
 
 const appRoutes: Routes = [
 	{path: '', component: HomeComponent},
-	{path: 'recipes', loadChildren: './recipes/recipes.module#RecipesModule'},
+	{path: 'recipes', loadChildren: './recipes/recipes.module#RecipesModule', canLoad: [AuthGuardService] },
 	{path: 'shopping-list', component: ShoppingListComponent},
 	{path: 'signup', component: SignupComponent},
 	{path: 'signin', component: SigninComponent}
--- src/app/shared/services/auth-guard.service.ts
+++ src/app/shared/services/auth-guard.service.ts
@@ -1,16 +1,29 @@
-import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot } from '@angular/router';
+import { ActivatedRouteSnapshot, CanActivate, CanLoad, Route, Router, RouterStateSnapshot } from '@angular/router';
 import { Injectable } from '@angular/core';
 import { AuthService } from './auth.service';
+import { Observable } from 'rxjs/index';
 
 @Injectable({
 	providedIn: 'root'
 })
-export class AuthGuardService implements CanActivate {
+export class AuthGuardService implements CanActivate, CanLoad {
 
-	constructor(private authService: AuthService) { }
+	constructor(private authService: AuthService,
+	            private router: Router) { }
 
 	canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean {
-
 		return this.authService.isAuthenticated();
+	}
+
+
+	canLoad(route: Route): Observable<boolean> | Promise<boolean> | boolean {
+		console.log("xxx");
+		const isAuthenticated = this.authService.isAuthenticated();
+		if (isAuthenticated) {
+			return true;
+		} else {
+			this.router.navigate(['/']);
+			return false;
+		}
 	}
 }
